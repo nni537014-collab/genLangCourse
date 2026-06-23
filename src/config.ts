199d7ab83ffe78066fc8a3024af1da67f42f8d65
Your Name <you@example.com>
2026-06-23 14:43:21 +0100

    audio file

--- src/config.ts
+++ src/config.ts
@@ -6,4 +6,5 @@ export const h5pAssetsDirName = "h5p";
 export const h5pContentDir = "content";
 export const h5pContentFileName = "content.json";
 export const h5pJsonFileName = "h5p.json";
+export const h5pAudioDirName = "audio"
  
