526f09340a2ca32b3a70ae85c7ac83f0c067dcc8
unknown <nni537014@gmail.com>
2026-06-24 13:45:42 +0000

    move

--- src/service/generators/blanks.ts
+++ src/service/generators/blanks.ts
@@ -8,12 +8,46 @@ import { md5Filename } from "./../utils.ts"
 
 export class BlanksGenerator implements ContentGenerator {
   generate(base: TranslationPair[], template: JsonValue): JsonValue {
-     return template;
-     /**
-     params
+    (template as any).questions = base.map(tp => {
+
+    })
+    return template;
+    /**
+    params
 "questions": ["<p>oslo is the capital of *norway*<\/p>"]
-    */
-    
+   */
+
+  }
+  generateBlank(tp: TranslationPair) {
+    //find longest word start and wrap in **
+    const parts = tp.translation.split(" ");
+    let longest: { index: number, length: number } = {
+      index: 0,
+      length: 0
+    }
+    const partInfo = parts.map((part, i) => {
+      const ret: any = {};
+      ret.length = part.length
+      //@todo improve regex
+      if (part.match('/[,.?!:;]$/')) {
+        ret.endPunc = true;
+        ret.length -= 1;
+      } if (part.match('/^[,.?!:;]/')) {
+        ret.length -= 1;
+        ret.startPunc = true;
+      }
+      if (longest.length < ret.length) {
+        longest.length = ret.length;
+        longest.index = i;
+      }
+
+    })
+    const partWrapper = (toWrap: string: )=>{
+
+    }
+    // wrap longest
+    parts[longest.index] = 
+
   }
   getSupportedLibrary(): string {
     return "H5P.Blanks"; //@todo 
