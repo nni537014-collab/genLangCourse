77cbfe54692a5cbd41771d710403f5879f02a58c
unknown <nni537014@gmail.com>
2026-06-27 13:51:36 +0000

    genset factory

--- src/service/gen_set_factory.ts
+++ src/service/gen_set_factory.ts
@@ -13,6 +13,11 @@ import { DialogCardsWriter } from "./writers/dialog_cards.ts";
 import { SingleChoiceSetWriter } from "./writers/single_choice_set.ts";
 import { MultiChoiceWriter } from "./writers/multi_choice.ts";
 import { DragTextWriter } from "./writers/drag_text.ts";
+import { MultiMediaChoiceH5pGenerator } from "./generators/h5p/multi_media_choice.ts";
+import { DialogCardsH5pGenerator } from "./generators/h5p/dialog_cards.ts";
+import { SingleChoiceSetH5pGenerator } from "./generators/h5p/single_choice_set.ts";
+import { MultiChoiceH5pGenerator } from "./generators/h5p/multi_choice.ts";
+import { DragTextH5pGenerator } from "./generators/h5p/drag_text.ts";
 export function createGenSet(libraryName: LibraryNames) {
     let genSet: GenSet;
 
@@ -34,35 +39,35 @@ export function createGenSet(libraryName: LibraryNames) {
         case "H5P.Dialogcards":
             genSet = {
                 content: new DialogCardsGenerator(),
-                h5p: new H5pGenerator(),
+                h5p: new DialogCardsH5pGenerator(),
                 writer: new DialogCardsWriter(getWritePath()),
             }
             break;
         case "H5P.SingleChoiceSet":
             genSet = {
                 content: new SingleChoiceSetGenerator(),
-                h5p: new H5pGenerator(),
+                h5p: new SingleChoiceSetH5pGenerator(),
                 writer: new SingleChoiceSetWriter(getWritePath()),
             }
             break;
         case "H5P.MultiChoice":
             genSet = {
                 content: new MultiChoiceGenerator(),
-                h5p: new H5pGenerator(),
+                h5p: new MultiChoiceH5pGenerator(),
                 writer: new MultiChoiceWriter(getWritePath()),
             }
             break;
         case "H5P.MultiMediaChoice":
             genSet = {
                 content: new MultiMediaChoiceGenerator(),
-                h5p: new H5pGenerator(),
+                h5p: new MultiMediaChoiceH5pGenerator(),
                 writer: new MultiMediaChoiceWriter(getWritePath()),
             }
             break;
         case "H5P.DragText":
             genSet = {
                 content: new DragTextGenerator(),
-                h5p: new H5pGenerator(),
+                h5p: new DragTextH5pGenerator(),
                 writer: new DragTextWriter(getWritePath()),
             }
             break;
