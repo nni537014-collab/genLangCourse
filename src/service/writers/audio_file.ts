0f78794a50a1e2f54f9ed7e847a6f0a1ebf7ee15
root <root@localhost.localdomain>
2026-07-01 09:14:35 +0000

    mv

--- src/service/writers/audio_file.ts
+++ src/service/writers/audio_file.ts
@@ -1,77 +1,66 @@
-import type { TranslationPair } from "./../../types/types.ts"
+import type { TranslationPair } from "./../../types/types.ts";
 
-
-import { paths, md5Filename, audioFileName } from "../../utils/paths.ts"
-import fs from "fs"
+import { paths, md5Filename, audioFileName } from "../../utils/paths.ts";
+import fs from "fs";
 import say from "say";
 import { execSync } from "child_process";
 import { fileURLToPath } from "url";
 
+export type FlatBase = Record<string, string>;
 class AudioFileCreator {
   //    state: "ready" | "busy" = "ready";
-  convertToMP3 = false;
   //    busyCount = 0;
-  busy: Record<string, TranslationPair> = {};
-  completed: TranslationPair[] = [];
-  voice = "Microsoft Hazel Desktop"
+  voice = "Microsoft Hazel Desktop";
   _base: TranslationPair[];
-  _flatBase: Record<string, string> = {}
-  constructor(base: TranslationPair[], asMP3: boolean) {
-    this.convertToMP3 = asMP3;
+  _flatBase: FlatBase = {};
+  constructor(base: TranslationPair[]) {
     this._base = base;
   }
-  flattenBase() {
-    this._base.forEach(tp => {
-      this._flatBase[this.busyKey("source", tp.source)] = tp.source;
-      this._flatBase[this.busyKey("translation", tp.translation)] = tp.translation;
-    })
+  flatBase() {
+    this._base.forEach((tp) => {
+      this._flatBase[this.key("source", tp.source)] = tp.source;
+      this._flatBase[this.key("translation", tp.translation)] = tp.translation;
+    });
+    return Object.entries(this._flatBase);
   }
-  produceAllFiles() {
-
+  produceAllFiles(allSettled: (stored: string[]) => void) {
     //        this._base.forEach(this.produceFile)
-    this.flattenBase();
-    const created: Promise<string>[] = [];
-    for (const [fileName, text] of Object.entries(this._flatBase)) {
-      created.push(this.createAudioFile(text, fileName))
+    const created: Promise<Record<string, string>>[] = [];
+    for (const [fileName, text] of this.flatBase()) {
+      created.push(this.createAudioFile(text, fileName));
     }
     const set = Promise.allSettled(created);
-
+    set.then(
+      (stored) => {
+        const values = stored
+          .map((value) => {
+            if (value.status === "fulfilled") {
+              return value.value;
+            } else {
+              return undefined;
+            }
+          })
+          .filter((value) => value);
+        allSettled(values);
+      },
+      (reason) => {
+        console.log(reason);
+      },
+    );
   }
-  /*
-  produceFile(tp: TranslationPair) {
-
-    const translationAudioPath = paths.getAudio(tp.translation);
-    const sourceAudioPath = paths.getAudio(tp.source);
-
-    if (!fs.existsSync(translationAudioPath) || fs.existsSync(sourceAudioPath)) {
-      //gen
-      this.busy[this.busyKey("source", tp.source)] = tp;
-      this.busy[this.busyKey("translation", tp.translation)] = tp;
-      //          this.state = "busy";
-      //             
-    }
-  }
-*/
   createAudioFile(text: string, filePath: string) {
-    const wavFile = `${fileURLToPath}.wav`
-    return new Promise<string>((resolve, reject) => {
+    const wavFile = `${filePath}.wav`;
+    return new Promise<Record<string, string>>((resolve, reject) => {
       say.export(text, this.voice, 1, wavFile, (err) => {
         if (err) reject(err);
         execSync(`ffmpeg -y -i ${wavFile} ${filePath}`);
-        resolve(filePath);
-        //     delete this.busy[this.busyKey(type, text)];
-        // if (Object.keys(this.busy).length === 0) {
-        //   //             this.state = "ready";
-        //   //       console.log("finished creating audio files")
-        //   if (this.convertToMP3) {
-        //     //        execSync(`ffmpeg -y -i ${wavFile} ${filePath}`);
-        //   }
-        // }
+        const res: Record<string, string> = {};
+        res[filePath] = text;
+        resolve(res);
       });
-
-    })
+    });
   }
-  busyKey(type: "source" | "translation", input: string) {
+  protected key(type: "source" | "translation", input: string) {
     return `${audioFileName(input, type)}`;
   }
 }
