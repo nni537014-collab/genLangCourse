4c7752a7f288c1e13e1e9caea10fea0e3558b61a
unknown <nni537014@gmail.com>
2026-07-10 11:59:27 +0000

    multimedia choice

--- src/service/generators/content/multi_media_choice.ts
+++ src/service/generators/content/multi_media_choice.ts
@@ -7,70 +7,46 @@ import type {
 
 } from "../../../types/types.ts";
 import { audioFileName } from "../../../utils/paths.ts"
-import type { MultiChoiceContent } from "../../../types/H5P/content/multi-choice.ts";
+import type { MultimediaChoiceContent, Option, OptionMedia } from "../../../types/H5P/content/multimedia-choice.ts";
 
 export class MultiMediaChoiceGenerator implements ContentGenerator {
   _question = {
-    prefix: "<h3>Select the matching audio<\/h3><p>",
-    postfix: "<\/p>"
+    prefix: "<h3>Select the matching audio</h3><p>",
+    postfix: "</p>"
   };
   //@todo types
-    generate(base: TranslationPair[], template: MultiChoiceContent): MultiChoiceContent[] {
+    generate(base: TranslationPair[], template: MultimediaChoiceContent): MultimediaChoiceContent[] {
      
      base.map((tp, i) => {
 
-       const templ = structuredClone(template);
-       templ.question = `${this._question.prefix}${tp.source}${this._question.postfix}`
+       const newTemplate = structuredClone(template);
+       newTemplate.question = `${this._question.prefix}${tp.source}${this._question.postfix}`
        
-       templ.media.type = this.generateMedia(tp, templ.media.type, "source");
-       templ.options = this.generateOptions(tp, i, base, templ.options);
+      //  newTemplate.media.type = this.generateMedia(tp, newTemplate.media.type, "source");
+       newTemplate.options = this.generateOptions(tp, i, base, newTemplate.options);
      });
      return [template];
   }
-  generateMedia(tp: TranslationPair, template: JsonValue, type: "source"|"translation"): JsonValue{
+  generateMedia(tp: TranslationPair, template: OptionMedia, type: "source"|"translation"): OptionMedia{
          
-         (template as any).subContentId = randomUUID(); //@todo check function - utils?
-         (template as any).params.files = [{
+         template.subContentId = randomUUID(); //@todo check function - utils?
+         template.params.files = [{
                           "path": this.generateFilePath(tp[type], type),
-                          "mime": "audio\/mpeg",
+                          "mime": "audio/mpeg",
                           "copyright": { "license": "U" }
                         }]
          return template;
-         /*
-           {       "type": {
-                    "params": {
-                      "playerMode": "minimalistic",
-                      "fitToWrapper": false,
-                      "controls": true,
-                      "autoplay": false,
-                      "playAudio": "Play audio",
-                      "pauseAudio": "Pause audio",
-                      "contentName": "Audio",
-                      "audioNotSupported": "Your browser does not support this audio",
-                      "files": [
-                        {
-                          "path": "audios\/files-6a3a495940dd5.mp3",
-                          "mime": "audio\/mpeg",
-                          "copyright": { "license": "U" }
-                        }
-                      ]
-                    },
-                    "library": "H5P.Audio 1.5",
-                    "metadata": {
-                      "contentType": "Audio",
-                      "license": "U",
-                      "title": "Untitled Audio"
-                    },
-                    "subContentId": "acfb0c69-b385-49b9-98e0-2cfb0c0ba790"
-                  },
-         */
   }
-  generateOptions(tp: TranslationPair, index: number, base: TranslationPair[], templ: JsonValue){
+  generateOptions(
+    tp: TranslationPair,
+    index: number, 
+    base: TranslationPair[], 
+    templ: Option[]){
     
     if(!Array.isArray(templ)) throw new Error("bad templ")
-    const mediaTempl = templ.pop() as any;
-    if(!mediaTempl || !mediaTempl.media) throw new Error("bad templ")
-    const ret: { media: any, correct: boolean}[] = [];
+    const optionTempl = templ.pop();
+    if(!optionTempl) throw new Error("bad templ")
+    const ret: Option[] = [];
     const optionBaseIndexes: number[] = [index];
     
     while(optionBaseIndexes.length <  3){
@@ -82,14 +58,14 @@ export class MultiMediaChoiceGenerator implements ContentGenerator {
 
     for(let i = 0; i < optionBaseIndexes.length; i++){
       const current = base[i];
-      if(!current) throw new Error("bad pairs")
-      ret.push({
-        media:this.generateMedia(
-            current,
-            structuredClone(mediaTempl.media),
-            "translation"),
-        correct: (i === 0)
-      })
+      if(!current) throw new Error("bad pairs");
+      const option = structuredClone(optionTempl);
+      option.media = this.generateMedia(
+        current,
+        structuredClone(optionTempl.media),
+        "translation");
+      option.correct = (i === 0);
+      ret.push(option);
     }
     return ret;
   }
@@ -99,69 +75,4 @@ export class MultiMediaChoiceGenerator implements ContentGenerator {
   getSupportedLibrary(): LibraryNames {
     return "H5P.MultiMediaChoice"; //@todo 
   }
-}
-
-/*
-"media": {
-                  "type": {
-                    "params": {
-                      "playerMode": "minimalistic",
-                      "fitToWrapper": false,
-                      "controls": true,
-                      "autoplay": false,
-                      "playAudio": "Play audio",
-                      "pauseAudio": "Pause audio",
-                      "contentName": "Audio",
-                      "audioNotSupported": "Your browser does not support this audio",
-                      "files": [
-                        {
-                          "path": "audios\/files-6a3a495940dd5.mp3",
-                          "mime": "audio\/mpeg",
-                          "copyright": { "license": "U" }
-                        }
-                      ]
-                    },
-                    "library": "H5P.Audio 1.5",
-                    "metadata": {
-                      "contentType": "Audio",
-                      "license": "U",
-                      "title": "Untitled Audio"
-                    },
-                    "subContentId": "acfb0c69-b385-49b9-98e0-2cfb0c0ba790"
-                  },
-                 
-  "question": "<h3>Select the matching audio<\/h3><p>this is a phrase<\/p>"
-"options": [
-                   {
-                    "media": {
-                      "params": {
-                        "playerMode": "minimalistic",
-                        "fitToWrapper": false,
-                        "controls": true,
-                        "autoplay": false,
-                        "playAudio": "Play audio",
-                        "pauseAudio": "Pause audio",
-                        "contentName": "Audio",
-                        "audioNotSupported": "Your browser does not support this audio",
-                        "files": [
-                          {
-                            "path": "audios\/files-6a3a489d935e2.mp3",
-                            "mime": "audio\/mpeg",
-                            "copyright": { "license": "U" }
-                          }
-                        ]
-                      },
-                      "library": "H5P.Audio 1.5",
-                      "metadata": {
-                        "contentType": "Audio",
-                        "license": "U",
-                        "title": "Untitled Audio"
-                      },
-                      "subContentId": "2a6f7667-a2d1-4253-bcbe-9058abba2d34"
-                    },
-                    "correct": true
-                  },
-                  {
-                    "media": {
-                     
-*/    
+} 
