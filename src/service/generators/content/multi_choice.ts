31f8be22bdcf30c7078bf42c1f65b786a9956693
unknown <nni537014@gmail.com>
2026-07-11 11:46:53 +0000

    cig

--- src/service/generators/content/multi_choice.ts
+++ src/service/generators/content/multi_choice.ts
@@ -8,7 +8,7 @@ import type {
 } from "../../../types/types.ts";
 import {  genRandomNumbers,  } from "../../../utils/utils.ts"
 import {  paths, } from "../../../utils/paths.ts"
-import type { MultiChoiceContent } from "../../../types/H5P/content/multi-choice.ts";
+import type { Answer, MultiChoiceContent } from "../../../types/H5P/content/multi-choice.ts";
 
 
 export class MultiChoiceGenerator implements ContentGenerator {
@@ -17,7 +17,8 @@ export class MultiChoiceGenerator implements ContentGenerator {
     //creating an array of structuredClone of template
 
     const ret: MultiChoiceContent[] = base.map(tp => {
-      const templClone = structuredClone(template) as any;
+      const templClone = structuredClone(template);
+      
       const file = templClone.media.type.params.files[0];
       if (typeof file !== "object") throw new Error("bad data");
       file.path = this.generateAudioPath(tp, "translation");
@@ -26,76 +27,13 @@ export class MultiChoiceGenerator implements ContentGenerator {
       templClone.answers = this.generateAnswers(tp, base, templClone.answers);
       return templClone;
     });
-    //media {: type: { params : { files {path:string}[] - path
-    //need path for one file - learning language phrase
+    return ret;
 
-    //question: string - just instruction to listen
-    //wrap in <p>
-
-    //answers {correct: boolean, text: string}[]
-    // text in source lang  - one correct ans - wrap <div>
-    // structuredClone required
-    return ret;//[];//template;
-
-
-    // const j = {
-    //   "media": {
-    //     "disableImageZooming": false,
-    //     "type": {
-    //       "params": {
-    //         "playerMode": "minimalistic",
-    //         "fitToWrapper": false,
-    //         "controls": true,
-    //         "autoplay": false,
-    //         "playAudio": "Play audio",
-    //         "pauseAudio": "Pause audio",
-    //         "contentName": "Audio",
-    //         "audioNotSupported": "Your browser does not support this audio",
-    //         "files": [
-    //           {
-    //             "path": "audios\/files-6a3a4467a3f91.mp3",
-    //             "mime": "audio\/mpeg",
-    //             "copyright": { "license": "U" }
-    //           }
-    //         ]
-    //       },
-    //       "library": "H5P.Audio 1.5",
-    //       "metadata": {
-    //         "contentType": "Audio",
-    //         "license": "U",
-    //         "title": "Untitled Audio"
-    //       },
-    //       "subContentId": "823aa57d-c970-4f38-a747-0ac17f4fb65f"
-    //     }
-    //   },
-    //   "answers": [
-    //     {
-    //       "correct": true,
-    //       "tipsAndFeedback": {
-    //         "tip": "",
-    //         "chosenFeedback": "",
-    //         "notChosenFeedback": ""
-    //       },
-    //       "text": "<div>correct answer<\/div>"
-    //     },
-    //     {
-    //       "correct": false,
-    //       "tipsAndFeedback": {
-    //         "tip": "",
-    //         "chosenFeedback": "",
-    //         "notChosenFeedback": ""
-    //       },
-    //       "text": "<div>wrong answer<\/div>"
-    //     }
-    //   ],
-    //   "question": "<p>Listen and then select the correct answer<\/p>"
-    // }
   }
-  generateAnswers(tp: TranslationPair, base: TranslationPair[], template: JsonValue) {
-    const ret: JsonValue[] = [];
+  generateAnswers(tp: TranslationPair, base: TranslationPair[], template: Answer[]) {
+    const ret: Answer[] = [];
     if (!Array.isArray(template)) throw new Error("bad data");
     const ansTempl = template[0];
-    if (!ansTempl) throw new Error("bad data");
     const skipIndex = base.findIndex(tpUnderTest => {
       return (tp.source === tpUnderTest.source &&
         tp.translation === tpUnderTest.translation
@@ -107,14 +45,14 @@ export class MultiChoiceGenerator implements ContentGenerator {
       0,
       base.length,
       [skipIndex]);
-    const correct = structuredClone(ansTempl) as { correct: boolean, text: string };
+    const correct = structuredClone(ansTempl);
     correct.correct = true;
     correct.text = `<div>${tp.source}</div>`
     ret.push(correct)
     for (const index of wrongIndexes) {
       const tpWrong = base[index];
       if (!tpWrong) throw new Error("bad data");
-      const wrong = structuredClone(ansTempl) as { correct: boolean, text: string };
+      const wrong = structuredClone(ansTempl);
       wrong.correct = false;
       wrong.text = `<div>${tpWrong.translation}</div>`;
       ret.push(wrong);
--- src/types/H5P/content/multi-choice.ts
+++ src/types/H5P/content/multi-choice.ts
@@ -65,6 +65,115 @@ export interface Confirm {
 
 export interface Media {
     disableImageZooming: boolean;
+    type:                Type;
+}
+
+export interface Type {
+    params:       Params;
+    library:      string;
+    metadata:     Metadata;
+    subContentId: string;
+}
+
+export interface Metadata {
+    contentType: string;
+    license:     string;
+    title:       string;
+}
+
+export interface Params {
+    playerMode:        string;
+    fitToWrapper:      boolean;
+    controls:          boolean;
+    autoplay:          boolean;
+    playAudio:         string;
+    pauseAudio:        string;
+    contentName:       string;
+    audioNotSupported: string;
+    files:             File[];
+}
+
+export interface File {
+    path:      string;
+    mime:      string;
+    copyright: Copyright;
+}
+
+export interface Copyright {
+    license: string;
+}
+
+export interface OverallFeedback {
+    from: number;
+    to:   number;
+}
+export interface MultiChoiceContent {
+    media:           Media;
+    answers:         Answer[];
+    overallFeedback: OverallFeedback[];
+    behaviour:       Behaviour;
+    UI:              UI;
+    confirmCheck:    Confirm;
+    confirmRetry:    Confirm;
+    question:        string;
+}
+
+export interface UI {
+    checkAnswerButton:  string;
+    submitAnswerButton: string;
+    showSolutionButton: string;
+    tryAgainButton:     string;
+    tipsLabel:          string;
+    scoreBarLabel:      string;
+    tipAvailable:       string;
+    feedbackAvailable:  string;
+    readFeedback:       string;
+    wrongAnswer:        string;
+    correctAnswer:      string;
+    shouldCheck:        string;
+    shouldNotCheck:     string;
+    noInput:            string;
+    a11yCheck:          string;
+    a11yShowSolution:   string;
+    a11yRetry:          string;
+}
+
+export interface Answer {
+    correct:         boolean;
+    tipsAndFeedback: TipsAndFeedback;
+    text:            string;
+}
+
+export interface TipsAndFeedback {
+    tip:               string;
+    chosenFeedback:    string;
+    notChosenFeedback: string;
+}
+
+export interface Behaviour {
+    enableRetry:                boolean;
+    enableSolutionsButton:      boolean;
+    enableCheckButton:          boolean;
+    type:                       string;
+    singlePoint:                boolean;
+    randomAnswers:              boolean;
+    showSolutionsRequiresInput: boolean;
+    confirmCheckDialog:         boolean;
+    confirmRetryDialog:         boolean;
+    autoCheck:                  boolean;
+    passPercentage:             number;
+    showScorePoints:            boolean;
+}
+
+export interface Confirm {
+    header:       string;
+    body:         string;
+    cancelLabel:  string;
+    confirmLabel: string;
+}
+
+export interface Media {
+    disableImageZooming: boolean;
 }
 
 export interface OverallFeedback {
